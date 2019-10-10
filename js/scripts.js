361fc3250b8b7d0da0e1f7dc106d4055aa332253
Gabriel Sa <gabrieljensensa@gmail.com>
2019-10-10 15:06:38 -0300

    Aula 6

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+$ (function () {
+    // abre e fecha menu
+    $ ('.nav-toggle, .nav-close').click(function(e) {
+        e.preventDefault();
+        $ ('.nav').toggleClass('active');
+    });
+});
