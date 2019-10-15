04a2ddd119b27194186b4fda018529f9b0fb1f53
Gabriel Sa <gabrieljensensa@gmail.com>
2019-10-15 17:44:37 -0300

    test10

--- curso_html/settings.py
+++ curso_html/settings.py
@@ -121,10 +121,3 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'pages/static'),
-]
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'pages/media')
-MEDIA_URL = '/media/'
