bc2ec85f724c6d94217139dec0ae585933eb6267
Gabriel Sa <gabrieljensensa@gmail.com>
2019-10-15 15:53:23 -0300

    test22

--- curso_html/settings.py
+++ curso_html/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'pages/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'pages/static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'pages/media')
