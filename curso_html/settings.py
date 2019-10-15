090dd318765f50a986c3ab572e45fd3410bb0400
Gabriel Sa <gabrieljensensa@gmail.com>
2019-10-15 16:27:29 -0300

    test5555

--- curso_html/settings.py
+++ curso_html/settings.py
@@ -125,7 +125,7 @@ STATICFILES_DIRS = (
 
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'pages/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'pages/media/')
 MEDIA_URL = '/media/'
 
 django_heroku.settings(locals())
