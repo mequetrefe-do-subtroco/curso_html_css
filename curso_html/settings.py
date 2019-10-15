5805c4f528e4ce4df49d4ce7f9e5ed7c2e74782f
Gabriel Sa <gabrieljensensa@gmail.com>
2019-10-15 17:52:06 -0300

    test11

--- curso_html/settings.py
+++ curso_html/settings.py
@@ -121,3 +121,6 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'pages/media')
+MEDIA_URL = '/media/'
--- curso_html/urls.py
+++ curso_html/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
 ]
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
